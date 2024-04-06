edb49f7a987decd747d4bcbd8f4f810ece5de848
AnnaKashyra <klimenko.anna@gmail.com>
2024-04-07 00:40:52 +0300

    formated document

--- exercises/lecture-28/app.js
+++ exercises/lecture-28/app.js
@@ -11,57 +11,9 @@
 
 // Скидання форми: після успішної реєстрації потрібно скинути значення полів форми.
 
-// const form = document.getElementById('registrationForm');
-// const errorMessages = document.getElementById('errorMessages');
-
-// form.addEventListener('submit', function(event) {
-//     event.preventDefault();
-
-//     errorMessages.innerHTML = '';
-
-//     const username = document.getElementById('username').value;
-//     const email = document.getElementById('email').value;
-//     const password = document.getElementById('password').value;
-
-//     if (username.trim() === '') {
-//         displayError('Username cannot be empty.');
-//         return;
-//     }
-
-//     const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-//     if (!emailRegex.test(email)) {
-//         displayError('Invalid email format.');
-//         return;
-//     }
-
-//     const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
-//     if (!passwordRegex.test(password)) {
-//         displayError('Password must contain at least 8 characters, including one uppercase letter, one lowercase letter, one digit, and one special character.');
-//         return;
-//     }
-
-//     alert('Form submitted successfully!');
-//     form.reset();
-// });
-
-// form.querySelectorAll('input').forEach(input => {
-//   input.addEventListener('input', function() {
-//       errorMessages.innerHTML = '';
-//       this.classList.remove('error');
-//   });
-// });
-
-// function displayError(message, inputName) {
-//     const errorMessage = document.createElement('div');
-//     errorMessage.textContent = message;
-//     errorMessages.appendChild(errorMessage);
-//     document.getElementById(inputName).classList.add('error');
-// }
-
 const form = document.getElementById("registrationForm");
 const errorMessages = document.getElementById("errorMessages");
 
-// Додаємо обробник події submit для форми
 form.addEventListener("submit", function (event) {
   event.preventDefault();
   errorMessages.innerHTML = "";
--- exercises/lecture-29/app.js
+++ exercises/lecture-29/app.js
@@ -5,22 +5,22 @@
 
 class AuthException extends Error {
   constructor(code, message) {
-      const fullMessage = message ? `${code}: ${message}` : code;
-      super(fullMessage) ;
-      this.name = code;
-      this.code = code;
-      this.message = fullMessage;
+    const fullMessage = message ? `${code}: ${message}` : code;
+    super(fullMessage);
+    this.name = code;
+    this.code = code;
+    this.message = fullMessage;
   }
 
   toString() {
-      return this.message;
+    return this.message;
   }
 }
 
 // Task 2
 // Створіть змінну checkAuth, яка дорівнює результату пошуку селектора з класом check-auth у файлі index.html.
 
-const checkAuth = document.querySelector('.check-auth');
+const checkAuth = document.querySelector(".check-auth");
 
 // Task 3
 // За допомогою addEventListener, виконайте обробку події click на змінній checkAuth. Функція зворотного зв'язку обробки цієї події повинна запускати блок try { } catch(e) {}.
@@ -28,24 +28,26 @@ const checkAuth = document.querySelector('.check-auth');
 // У блоці catch() перехопити цей виняток та передати сформоване повідомлення про помилку у діалог dialogBoxId, відобразивши його у параграфі з класом ="message".
 // Якщо результат повернення функції isAuth() дорівнює true, за допомогою метода window.open відкрити сторінку success.html.
 
-const isAuth = (auth)  => auth ?? false;
-const dialogBoxId=document.getElementById("dialogBox");
+const isAuth = (auth) => auth ?? false;
+const dialogBoxId = document.getElementById("dialogBox");
 
-checkAuth.addEventListener('click', function() {
+checkAuth.addEventListener("click", function () {
   try {
-      if (!isAuth()) {
-          throw new AuthException('FORBIDDEN', "You don't have access to this page");
-      } else {
-          window.open('success.html');
-      }
+    if (!isAuth()) {
+      throw new AuthException(
+        "FORBIDDEN",
+        "You don't have access to this page"
+      );
+    } else {
+      window.open("success.html");
+    }
   } catch (e) {
-      const messageElement = dialogBoxId.querySelector('.message');
-      messageElement.textContent = e.message;
-      dialogBoxId.showModal();
+    const messageElement = dialogBoxId.querySelector(".message");
+    messageElement.textContent = e.message;
+    dialogBoxId.showModal();
   }
 });
 
-function closeDialog(){
-  dialogBoxId.close(); 
+function closeDialog() {
+  dialogBoxId.close();
 }
-
