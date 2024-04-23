c2f174af1237f51a753b5c3e89b80167621981e8
AnnaKashyra <klimenko.anna@gmail.com>
2024-04-23 14:06:35 +0300

    added react-03

--- blog/src/App.tsx
+++ blog/src/App.tsx
@@ -6,28 +6,19 @@ import "./App.css";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import Gallery from './components/Gallery';
+import Blog from './components/Blog';
 
-const jsxElement = <h1>I am a JSX element</h1>;
-const header = (
-	<header>
-		<h1>Welcome to React</h1>
-		<h2>Getting Started React</h2>
-		<h3>JavaScript Library</h3>
-	</header>
-);
-
-const fragment = (
-	<>
-		<h1>Welcome to React</h1>
-		<h2>Getting Started React</h2>
-		<h3>JavaScript Library</h3>
-	</>
-);
-
-const footer = <p>Copyright &copy; 2024</p>;
 
 function App() {
 	const [count, setCount] = useState(0);
+
+  let post = {
+    id: 1,
+    title: "Programming Algorithm",
+    content: `The word Algorithm means “a process or set of rules to be followed in calculations or other problem-solving operations”. Therefore Algorithm refers to a set of rules/instructions that step-by-step define how a work is to be executed upon in order to get the expected results.`,
+    cover: "/vite.svg",
+    likes: 5
+  };
 
 	return (
 		<>
@@ -41,6 +32,9 @@ function App() {
 				</a>
 			</div>
 			<h1>Vite + React</h1>
+
+      <Blog post={post} key={post.id} />
+
 			<div className="card">
 				<button onClick={() => setCount((count) => count + 1)}>
 					count is {count}
@@ -49,11 +43,6 @@ function App() {
 					Edit <code>src/App.tsx</code> and save to test HMR
 				</p>
 			</div>
-
-			{jsxElement}
-			{header}
-			{fragment}
-			{footer}
 
       <div>
         <Gallery />
